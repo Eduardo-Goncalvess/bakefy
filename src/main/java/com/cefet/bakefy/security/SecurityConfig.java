4f4881a6561bdb8533e7a9b40158d9c22987dce1
MavenWysserah <edusilvarocha@hotmail.com>
2026-08-22 20:11:38 -0300

    Correção2

--- src/main/java/com/cefet/bakefy/security/SecurityConfig.java
+++ src/main/java/com/cefet/bakefy/security/SecurityConfig.java
@@ -29,20 +29,28 @@ public class SecurityConfig {
             )
 
             .authorizeHttpRequests(auth -> auth
+
+                // Login e cadastro
                 .requestMatchers(
                     "/api/v1/usuarios/login",
                     "/api/v1/clientes",
                     "/api/v1/empresas"
                 ).permitAll()
+
+                // Consultas públicas
                 .requestMatchers(
                     HttpMethod.GET,
                     "/api/v1/produtos",
                     "/api/v1/produtos/**"
                 ).permitAll()
+
+                // Requisições CORS
                 .requestMatchers(
                     HttpMethod.OPTIONS,
                     "/**"
                 ).permitAll()
+
+                // Todo o restante exige JWT
                 .anyRequest().authenticated()
             )
 
