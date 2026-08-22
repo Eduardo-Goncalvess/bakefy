ad017c16712021fa7d93671f1a1f884f20731102
MavenWysserah <edusilvarocha@hotmail.com>
2026-08-22 19:45:35 -0300

    Adiciona autenticacao JWT e corrige JwtAuthFilter.java

--- src/main/java/com/cefet/bakefy/security/JwtAuthFilter.java
+++ src/main/java/com/cefet/bakefy/security/JwtAuthFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -39,14 +40,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String metodo = request.getMethod();
         String caminho = request.getRequestURI();
 
-        if ("OPTIONS".equalsIgnoreCase(metodo)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+        boolean requisicaoPublica =
+                "GET".equalsIgnoreCase(metodo)
+                || "OPTIONS".equalsIgnoreCase(metodo)
+                || ("POST".equalsIgnoreCase(metodo)
+                && ROTAS_PUBLICAS_POST.contains(caminho));
 
-        if ("POST".equalsIgnoreCase(metodo)
-                && ROTAS_PUBLICAS_POST.contains(caminho)) {
-
+        if (requisicaoPublica) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -79,7 +79,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                     new UsernamePasswordAuthenticationToken(
                             idUsuario,
                             null,
-                            List.of()
+                            AuthorityUtils.NO_AUTHORITIES
                     );
 
             SecurityContextHolder
@@ -92,6 +92,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                     HttpServletResponse.SC_UNAUTHORIZED,
                     "Token inválido ou expirado."
             );
+
             return;
         }
 
