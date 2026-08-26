664c42b81142447f1d62bc0561af700f37c037b3
MavenWysserah <edusilvarocha@hotmail.com>
2026-08-25 23:12:11 -0300

    Sem @Data

--- src/main/java/com/cefet/bakefy/entity/Produto.java
+++ src/main/java/com/cefet/bakefy/entity/Produto.java
@@ -10,11 +10,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Data
 @EqualsAndHashCode(of = "idProduto")
 @NoArgsConstructor
 @AllArgsConstructor
